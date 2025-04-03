3cfa49bb7fbfa9abfc8cb3d647d060ef4657c86e
zalbuquerque <zaraalb7@gmail.com>
2025-04-03 17:12:44 -0300

    add tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String meuNome = "Zara";
+
+        double salarioMinimo = 2500;
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
